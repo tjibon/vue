1482afb57a6c26c08ff406196f895c6ee94ab7b7
tjibon <41022171+tjibon@users.noreply.github.com>
2022-10-03 22:55:48 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// ..............Custom JavaScript Code.................
+$("#leftside-navigation .sub-menu > a").click(function(e) {
+    $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+    e.stopPropagation()
+  });
+
+  jQuery(function($){
+    
+    
+  });
+ 
